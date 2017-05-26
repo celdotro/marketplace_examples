a115c6e4d411548d5c91bec72b868fffd60bbdff
Florin D <fredtux@users.noreply.github.com>
2017-05-26 13:17:10 +0300

    Documentation for api_include 	modified:   api_include.php

--- api_include.php
+++ api_include.php
@@ -1,6 +1,7 @@
 <?php
 /**
- *  index.php => define USER and PASS constants using the supplied username and password for your account
+ *  1. Change include path
+ *  2. index.php => define USER and PASS constants using the supplied username and password for your account
  */
 
-include '/home/pp4/Desktop/dev/folder_la_toti/alexm/repo_intern/marketplace/index.php';
+include 'PATH_TO/marketplace/index.php';
