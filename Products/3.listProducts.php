bd5a60f597327e12035f76d536e4849b7f50da1d
Florin D <fredtux@users.noreply.github.com>
2017-05-26 16:29:52 +0300

    Added list products example 	new file:   Products/3.listProducts.php

---
+++ Products/3.listProducts.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * [RO] Returneaza datele aferente produselor al caror nume respecta un anumit tipar. Lista este limitata cu start si limit, dar contine si un camp cu numarul total de astfel de produse.
+ * [EN] Returns product data for all products whose names are validated by a pattern. The returned list is limited by a start and a limit value, but includes the total number of products as a different field.
+ */
+include __DIR__ . '/../api_include.php';
+
+use celmarket\Products\ProductsList;
+
+#12#
+#FUNCTION#
+function listProducts($start, $limit, $search = null){
+    $object = new ProductsList();
+
+    try{
+        $response = $object->listProducts($start, $limit, $search);
+        print_r($response);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+#EXAMPLE#
+listProducts(0, 5, 'word1 word2');
