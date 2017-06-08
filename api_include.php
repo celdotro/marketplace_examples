577500083cd9ff50fc53102b5388c26f4a3909b7
Florin D <fredtux@users.noreply.github.com>
2017-06-08 11:19:17 +0300

    Exhaustive instructions 	modified:   api_include.php

--- api_include.php
+++ api_include.php
@@ -1,5 +1,11 @@
 <?php
 /**
- *  Use composer install to install dependencies
+ * [RO]
+ * Folositi composer pentru a instala dependetele:
+ *      - rulati urmatoarea comanda in directorul principal (directorul in care acest fisier este localizat): composer install
+ *
+ * [EN]
+ * Use composer to install dependencies:
+ *      - issue this command in the main folder (the folder in which this file is located): composer install
  */
 include 'vendor/autoload.php';
