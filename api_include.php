e8b5ca40a9a2cfda7ae7f1f49b5586458abe2acb
Florin D <fredtux@users.noreply.github.com>
2017-06-13 13:11:02 +0300

    Added WIKI links to first page in api_include 	modified:   api_include.php

--- api_include.php
+++ api_include.php
@@ -3,9 +3,11 @@
  * [RO]
  * Folositi composer pentru a instala dependetele:
  *      - rulati urmatoarea comanda in directorul principal (directorul in care acest fisier este localizat): composer install
+ * WIKI : https://github.com/celdotro/marketplace/wiki/Prima-pagina
  *
  * [EN]
  * Use composer to install dependencies:
  *      - issue this command in the main folder (the folder in which this file is located): composer install
+ * WIKI : https://github.com/celdotro/marketplace/wiki/First-page
  */
 include 'vendor/autoload.php';
