2ab0117354725a0b87b80d4b37b6d7b670083727
Florin D <fredtux@users.noreply.github.com>
2017-06-16 12:15:26 +0300

    Added comments 	modified:   index.php

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
+/**
+ * Get all PHP files from the specified path (all files that have the .php extension)
+ *
+ * @param $path
+ * @return void
+ */
 function getPHPFiles($path){
     return glob($path . '/*.php');
 }
 
+/**
+ * Get all folders from the current directory and exclude those specified in the parameter's regex string
+ *
+ * @param $excludeFilter
+ * @return void
+ */
 function getFolders($excludeFilter = NULL){
     $folderList = glob('*', GLOB_ONLYDIR );
     
@@ -12,14 +24,15 @@ function getFolders($excludeFilter = NULL){
         return preg_grep($excludeFilter, $folderList, PREG_GREP_INVERT);
 }
 
+// Get all folders from the current directory, but exclude the "vendor" folder
 $folders = getFolders('/vendor/');
 ?>
 <html>
     <body>
-    <?php foreach($folders as $folder) { ?>
+    <?php foreach($folders as $folder) { // Iterate through the folders list ?>
         <h2><?=$folder?></h2>
         <ul>
-            <?php foreach(getPHPFiles($folder) as $file) { ?>
+            <?php foreach(getPHPFiles($folder) as $file) { // Get all PHP files of the current folder, add a link to them and exlcude the folder path from their name?>
             <li><a href=<?=$file?>><?=str_replace($folder . '/', '', $file)?></a></li>
             <?php } ?>
         </ul>
