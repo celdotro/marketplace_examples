3ee28ef85c9afef013b9397d856f58ed93640927
Florin D <fredtux@users.noreply.github.com>
2017-06-19 13:43:52 +0300

    Added PHP interface check 	modified:   index.php

--- index.php
+++ index.php
@@ -26,6 +26,17 @@ function getFolders($excludeFilter = NULL){
 
 // Get all folders from the current directory, but exclude the "vendor" folder
 $folders = getFolders('/vendor/');
+
+// Determine which type of interface PHP is using and optimize output
+if(php_sapi_name() == 'cli'){ // Manually structured output
+    foreach($folders as $key => $folder){ // Iterate through the folders list
+        echo '[' . ($key + 1 ) . '] ' . $folder . ": {\n"; // Add folder and index (start from 1)
+        foreach(getPHPFiles($folder) as $file){ // Get each PHP file in the current folder
+            echo "\t" . __DIR__ . '/' . $file . "\n"; // Add the full path for the current file
+        }
+        echo "}\n\n"; // Add one blank row
+    }
+} else { // Output as HTML
 ?>
 <html>
     <body>
@@ -39,3 +50,4 @@ $folders = getFolders('/vendor/');
     <?php } ?>
     </body>
 </html>
+<?php } ?>
