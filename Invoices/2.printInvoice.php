083548207a8951424dbde31125cb17b48bdb2d35
Florin D <fredtux@users.noreply.github.com>
2017-06-19 17:38:25 +0300

    Removed 'check' parameter 	modified:   Invoices/2.printInvoice.php

--- Invoices/2.printInvoice.php
+++ Invoices/2.printInvoice.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * [RO] Returneaza o factura in format PDF pentru o comanda specificata prin parametru. Optional, se poate verifica stocul existent ianinte de tiparire. (https://github.com/celdotro/marketplace/wiki/Printare-factura)
- * [EN] Returns an invoice as a PDF file for an order specified as a parameter. Optionally it checks the existing product stock before printing. (https://github.com/celdotro/marketplace/wiki/Print-Invoice)
+ * [RO] Returneaza o factura in format PDF pentru o comanda specificata prin parametru (https://github.com/celdotro/marketplace/wiki/Printare-factura)
+ * [EN] Returns an invoice as a PDF file for an order specified as a parameter (https://github.com/celdotro/marketplace/wiki/Print-Invoice)
  */
 include __DIR__ . '/../api_include.php';
 
@@ -12,11 +12,11 @@ use celmarket\Invoices\InvoicesPrint;
 
 #24#
 #FUNCTION#
-function printInvoice($cmd, $check = null){
+function printInvoice($cmd){
     $object = new InvoicesPrint();
 
     try{
-        $response = $object->printInvoice($cmd, $check = null);
+        $response = $object->printInvoice($cmd);
         print_r($response);
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -24,4 +24,4 @@ function printInvoice($cmd, $check = null){
 }
 
 #EXAMPLE#
-printInvoice(123456, false);
+printInvoice(123456);
