5397fe272ece3703c36d06c173da022bf2a6b969
Florin D <fredtux@users.noreply.github.com>
2017-06-19 12:45:40 +0300

    Fixed translation 	modified:   Products/1.getCategories.php

--- Products/1.getCategories.php
+++ Products/1.getCategories.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * [RO] Returneaza lista de categorii disponibile. Prima categorie va avea implicit numele "parent". (https://github.com/celdotro/marketplace/wiki/Listeaza-categorii)
- * [EN] Returns the list of available categories. The first category will have the name 'parent' by default. (https://github.com/celdotro/marketplace/wiki/List-categories)
+ * [EN] Returns the list of available categories. The first category will be named 'parent' by default. (https://github.com/celdotro/marketplace/wiki/List-categories)
  */
 include __DIR__ . '/../api_include.php';
 
