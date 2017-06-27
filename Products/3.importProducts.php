0ff69b7a01291750d2c38898b2ae95739716595b
Florin D <fredtux@users.noreply.github.com>
2017-06-27 13:27:03 +0300

    Added parameter to import array 	modified:   Products/3.importProducts.php

--- Products/3.importProducts.php
+++ Products/3.importProducts.php
@@ -67,7 +67,8 @@ $arrProducts = array(
      array(
         'products_model' => 'Model2', // Obligatoriu pentru forma scurta / Mandatory for short form
         'pretf' => 203, // Trebuie cel putin 1 camp dintre 'pretf' si 'stoc' / At least one field between 'pretf' and 'stoc' is necessary
-        'stoc' => 38 // Trebuie cel putin 1 camp dintre 'pretf' si 'stoc' / At least one field between 'pretf' and 'stoc' is necessary
+        'stoc' => 38, // Trebuie cel putin 1 camp dintre 'pretf' si 'stoc' / At least one field between 'pretf' and 'stoc' is necessary
+        'hasTVA' => 0 // Are valoarea 0 sau nu exista daca pretf NU include TVA si 1 daca include / Has the value of 0 or it doesn't exist if pretf DOES NOT have the VAT tax included or 1 otherwise
     )
 
     // [... N]      
