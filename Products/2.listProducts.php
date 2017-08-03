21f54ee4f8672546ba5fc9a3b965b9ec54a39e21
Florin D <fredtux@users.noreply.github.com>
2017-08-03 11:44:03 +0300

    Added limit bypass for products listing 	modified:   Products/2.listProducts.php

--- Products/2.listProducts.php
+++ Products/2.listProducts.php
@@ -12,11 +12,11 @@ use celmarket\Products\ProductsList;
 
 #12#
 #FUNCTION#
-function listProducts($start, $limit, $search = null, $forceCount = false){
+function listProducts($start, $limit, $search = null, $forceCount = false, $bypassLimit = false){
     $object = new ProductsList();
 
     try{
-        $response = $object->listProducts($start, $limit, $search, $forceCount);
+        $response = $object->listProducts($start, $limit, $search, $forceCount, $bypassLimit);
         print_r($response);
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -24,4 +24,4 @@ function listProducts($start, $limit, $search = null, $forceCount = false){
 }
 
 #EXAMPLE#
-listProducts(1, 5, 'word1 word2', true);
+listProducts(1, 5, 'word1 word2', true, false);
