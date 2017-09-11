381796d416b4646bb798f635c90f1ff5976c5914
Florin D <fredtux@users.noreply.github.com>
2017-09-11 13:43:23 +0300

    Added comments to order reactivation

--- Orders/12.reactivateOrder.php
+++ Orders/12.reactivateOrder.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Reactiveaza o comanda anulata (https://github.com/celdotro/marketplace/wiki/Reactiveaza-comanda)
+ * [EN] Reactivates a cancelled order (https://github.com/celdotro/marketplace/wiki/Reactivate-order)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
