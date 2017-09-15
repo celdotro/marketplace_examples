fac5903555c45c6214063a1f19ff2bf8e2335ef2
Florin D <fredtux@users.noreply.github.com>
2017-09-15 15:40:37 +0300

    Added comments to function

--- Products/13.addOfferToExistingProduct.php
+++ Products/13.addOfferToExistingProduct.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Adauga o noua oferta unui produs existent (https://github.com/celdotro/marketplace/wiki/Adauga-o-noua-oferta-unui-produs-existent)
+ * [EN] Add another offer to an existing product (https://github.com/celdotro/marketplace/wiki/Add-offer-to-existing-product)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
