ddb0b1c1eee4c7002bcfa60b36594224a5aece79
Florin D <fredtux@users.noreply.github.com>
2017-10-31 14:24:18 +0200

    Added comments to service requests retrieval

--- Admin/12.getServiceRequests.php
+++ Admin/12.getServiceRequests.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Preia cererile de service sau retur ale clientilor (https://github.com/celdotro/marketplace/wiki/Preia-cererile-de-service)
+ * [EN] Retrieve service or return requests (https://github.com/celdotro/marketplace/wiki/Retrieve-service-requests)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
