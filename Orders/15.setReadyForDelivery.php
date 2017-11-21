95253c60f2748d9b367625114e9d64b677c631fc
Florin D <fredtux@users.noreply.github.com>
2017-11-21 11:44:25 +0200

    Added set order as ready for delivery

--- Orders/15.setReadyForDelivery.php
+++ Orders/15.setReadyForDelivery.php
@@ -1,6 +1,7 @@
 <?php
 /**
- *
+ * [RO] Seteaza o comanda ca fiind gata de livrare (https://github.com/celdotro/marketplace/wiki/Seteaza-gata-de-livrare)
+ * [EN] Set an order as ready for delivery (https://github.com/celdotro/marketplace/wiki/Set-order-as-ready-for-delivery)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
