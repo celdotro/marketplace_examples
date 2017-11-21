0614ee69e34c3f9d108572b8aaeb69a58134e6d9
Florin D <fredtux@users.noreply.github.com>
2017-11-21 17:23:00 +0200

    Added relevant comments

--- AWB/4.statusAwb.php
+++ AWB/4.statusAwb.php
@@ -1,10 +1,7 @@
 <?php
 /**
-<<<<<<< HEAD
  * [RO] Returneaza statusul unui AWB (https://github.com/celdotro/marketplace/wiki/Status-AWB)
  * [EN] Returns information about an AWB's status (https://github.com/celdotro/marketplace/wiki/AWB-Status)
-=======
->>>>>>> 35b7f8ca730692febb6fcc50d603ea3edef92a02
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
