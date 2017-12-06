718411fd440f89c3c5f2b87d09dfcd15b602a148
Florin D <fredtux@users.noreply.github.com>
2017-12-06 12:31:40 +0200

    Added import invoice

---
+++ Orders/16.importInvoice.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ */
+include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
+
+
+
+
+use celmarket\Orders\OrdersUpdate;
+
+#4#
+#FUNCTION#
+function importInvoice($cmd, $serie, $nr_fact){
+    $object = new OrdersUpdate();
+
+    try{
+        $response = $object->importInvoice($cmd, $serie, $nr_fact);
+        print_r($response);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+#EXAMPLE#
+importInvoice(123456, 'AA', 100);
