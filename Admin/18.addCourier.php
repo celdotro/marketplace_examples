4920e4cda6e8bdf0f0b6c976e03f1fa519438430
Florin D <fredtux@users.noreply.github.com>
2017-12-27 15:52:18 +0200

    Added courier addition

--- Admin/18.addCourier.php
+++ Admin/18.addCourier.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Trimite email pentru adaugarea unui nou curier (https://github.com/celdotro/marketplace/wiki/Adauga-curier)
+ * [EN] Sends an email with a request to add a new courier (https://github.com/celdotro/marketplace/wiki/Add-courier)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
