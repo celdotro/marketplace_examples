f9c6731521a297099b4863ad95e7714f40ce3c33
Florin D <fredtux@users.noreply.github.com>
2018-02-02 14:17:49 +0200

    Added order cancellation

---
+++ Orders/18.stornareComanda.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ */
+include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
+
+
+
+use celmarket\Orders\OrdersStatus;
+
+#15#
+#FUNCTION#
+function stornareComanda($orders_id){
+    $object = new OrdersStatus();
+
+    try{
+        $response = $object->stornareComanda($orders_id);
+        print_r($response);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+#EXAMPLE#
+$orders_id = 123456;
+stornareComanda($orders_id);
