4c7b291911f7c0777acfcdedd39c0796db8ae87d
Florin D <fredtux@users.noreply.github.com>
2018-04-24 15:39:07 +0300

    Added group_by_status

--- Orders/2.listOrders.php
+++ Orders/2.listOrders.php
@@ -49,6 +49,7 @@ $arrOptions = array(
     ),
     'order'  =>   0, // lipsa elementului sau prezenta sa cu valoarea null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
     'invoice'  =>  0, // lipsa elementului sau prezenta sa cu valoarea null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
-    'order_status' => 11 // lipsa elementului sau prezenta sa cu valoarea null, 0, 99 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0, 99 or empty string, then it won't be used as a filter
+    'order_status' => 11, // lipsa elementului sau prezenta sa cu valoarea null, 0, 99 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0, 99 or empty string, then it won't be used as a filter
+    'group_by_status' => true // lipsa elementului sau prezenta sa cu valoarea true va determina gruparea in functie de status, valoarea false va determina lispsa gruparii in functie de status / if the element is missing or its value is true, it will group by status, otherwise it will not group by any field
 );
 getOrdersList($start, $limit, $arrOptions, $status);
