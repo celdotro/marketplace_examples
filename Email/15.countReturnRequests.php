f5ccd299733eb9fbf62a194ce3a60990162cafbc
Florin D <fredtux@users.noreply.github.com>
2018-08-10 17:09:58 +0300

    1

--- Email/15.countReturnRequests.php
+++ Email/15.countReturnRequests.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Preia numarul de cereri de retur la care nu s-a raspuns (https://github.com/celdotro/marketplace/wiki/Numara-cererile-de-retur)
+ * [EN] Retrieves the number of unanswered return requests (https://github.com/celdotro/marketplace/wiki/Count-return-requests)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
