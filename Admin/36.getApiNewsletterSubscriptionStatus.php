0c540bd8cec971668c54a85b7c8e6456af4124a8
Florin D <fredtux@users.noreply.github.com>
2018-08-16 18:15:56 +0300

    1

--- Admin/36.getApiNewsletterSubscriptionStatus.php
+++ Admin/36.getApiNewsletterSubscriptionStatus.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Preia statusul abonarii la newsletter-ul pentru API (https://github.com/celdotro/marketplace/wiki/Status-abonare-newsletter-API)
+ * [EN] Retrieves the subscription status to API newsletter
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
