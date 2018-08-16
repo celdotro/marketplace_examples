b0e47525740c911e56f8fc18a69d196a4db118f0
Florin D <fredtux@users.noreply.github.com>
2018-08-16 18:02:29 +0300

    1

--- Admin/35.removeSubscriptionToApiNewsletter.php
+++ Admin/35.removeSubscriptionToApiNewsletter.php
@@ -1,5 +1,7 @@
 <?php
 /**
+ * [RO] Dezabonare de la newsletter-ul despre API (https://github.com/celdotro/marketplace/wiki/Dezabonare-de-la-newsletter-API)
+ * [EN] Unsubscribe from newsletter about API (https://github.com/celdotro/marketplace/wiki/Unsubscribe-from-API-newsletter)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
