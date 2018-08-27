2173dc14079662ebac0e2a3140957046e29d26f6
Florin D <fredtux@users.noreply.github.com>
2018-08-27 15:00:25 +0300

    Added account data

--- Admin/4.addAddress.php
+++ Admin/4.addAddress.php
@@ -12,11 +12,11 @@ use celmarket\Admin\AdminAddress;
 
 #0#
 #FUNCTION#
-function addAddress($address, $courier){
+function addAddress($address, $courier, $accData){
     $object = new AdminAddress();
 
     try{
-        $response = $object->addAddress($address, $courier);
+        $response = $object->addAddress($address, $courier, $accData);
         print_r($response);
     } catch (Exception $e) {
         echo $e->getMessage();
@@ -26,5 +26,6 @@ function addAddress($address, $courier){
 #EXAMPLE#
 $address = 'Str. X...';
 $courier = 'Courier Name';
+$accData = array();
 
-addAddress($address, $courier);
+addAddress($address, $courier, $accData);
