ca6a664f910230ee3ebd134888732fb3db7b9a7d
Florin D <fredtux@users.noreply.github.com>
2018-08-30 13:07:51 +0300

    Updated documentation link for the English version of order email list retrieving

--- Email/1.getOrderEmailList.php
+++ Email/1.getOrderEmailList.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * [RO] Returneaza o lista cu ID-uri si denumirea mesajelor predefinite pentru anumite actiuni legate de comanda (https://github.com/celdotro/marketplace/wiki/Listare-email-uri-predefinite-pentru-comenzi)
- * [EN] Returns a list of IDs and names of predefined emails for actions related to an order (https://github.com/celdotro/marketplace/wiki/Get-client-emails-for-an-order)
+ * [EN] Returns a list of IDs and names of predefined emails for actions related to an order (https://github.com/celdotro/marketplace/wiki/Predefined-email-list-for-orders)
  */
 include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
 
