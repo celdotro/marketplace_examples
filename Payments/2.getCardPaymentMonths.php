47a52cf4893cdc43df812203b207a5014aa356e0
Florin D <fredtux@users.noreply.github.com>
2018-09-14 18:07:24 +0300

    1

---
+++ Payments/2.getCardPaymentMonths.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * [RO] Preia lunile aferente platilor cu cardul
+ * [EN] Retrieve card payment months
+ */
+include __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'api_include.php';
+
+
+
+
+use celmarket\Payments\PaymentsReports;
+
+#25#
+#FUNCTION#
+function getCardPaymentMonths(){
+    $object = new PaymentsReports();
+
+    try{
+        $response = $object->getCardPaymentMonths();
+        print_r($response);
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
+
+#EXAMPLE#
+getCardPaymentMonths();
