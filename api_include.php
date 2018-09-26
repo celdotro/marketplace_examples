c6b283afbfe76624c973930d547215a56cf01fe1
Florin D <fredtux@users.noreply.github.com>
2018-09-26 15:40:41 +0300

    1

--- api_include.php
+++ api_include.php
@@ -21,8 +21,10 @@ include __DIR__ . '/../marketplace/index.php';
 // Authenticate - change USERNAME and PASSWORD with your credentials
 use celmarket\Auth;
 // Auth::setUserDetails('flo2', '0OZ1LEQC0QShKn24uM3KYrVYvdm9ZktPo2EDpke30hE3xsDFmm');
-Auth::setUserDetails('testdoi', '1Pass__)');
+// Auth::setUserDetails('testdoi', '1Pass__)');
 // Auth::setUserDetails('unfurnizor', '0OZ1LEQC0QShKn24uM3KYrVYvdm9ZktPo2EDpke30hE3xsDFmm');
 // Auth::setUserDetails('nichiduta', '0OZ1LEQC0QShKn24uM3KYrVYvdm9ZktPo2EDpke30hE3xsDFmm');
 // Auth::setUserDetails('alecoair', 'test');
 // Auth::setUserDetails('itelgsm', 'mbPeLK7wx5');
+
+Auth::setUserDetails('neoallbet', '0OZ1LEQC0QShKn24uM3KYrVYvdm9ZktPo2EDpke30hE3xsDFmm');
