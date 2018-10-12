b919c7a041a68b0819369fcb7fd1de3a74f1d35b
Florin D <fredtux@users.noreply.github.com>
2018-10-12 15:13:26 +0300

    Added changes to orders

--- Orders/2.listOrders.php
+++ Orders/2.listOrders.php
@@ -28,25 +28,30 @@ $start = 0;
 $limit = 10;
 $arrOptions = array(
     'date'  =>   array(
-        'from'  =>   '2017-05-05',
-        'to'  =>  '2017-05-31 23:59:59'
+        'minDate'  =>   '2018-09-07 08:39:42',
+        'maxDate'  =>  '2018-09-07 08:39:42',
     ),
-    'customer'  =>   array(
-        array( // Semne acceptate / Accepted signs: ee, ne
-            'name'  =>  'nume1', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
-            'sign'  =>  'ee'
-        ),
-        array(
-            'name'  =>  'nume2',
-            'sign'  =>  'ne'
-        )
+    'customer'  =>  'Florin Test Florin Test', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
+    'customerSign'  =>  'ee',
+    'product'  =>   'laptop', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
+    'productSign'  =>  'ee'
+    // 'order'  =>   2000542, // lipsa elementului sau prezenta sa cu valoarea null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
+    // 'invoice'  =>  0, // lipsa elementului sau prezenta sa cu valoarea null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
+    // 'order_status' => 2, // lipsa elementului sau prezenta sa cu valoarea null, 0, 99 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0, 99 or empty string, then it won't be used as a filter
+    // 'group_by_status' => true, // lipsa elementului sau prezenta sa cu valoarea true va determina gruparea in functie de status, valoarea false va determina lispsa gruparii in functie de status / if the element is missing or its value is true, it will group by status, otherwise it will not group by any field
+    // 'order_by_date_purchased' => 'DESC',
+    // 'min_order' => 113637
+    // 'payment_method' => 'Numerar sau ramburs'
+);
+$arrOptions = array(
+    'date'  =>   array(
+        'minDate'  =>   '2017-01-01 00:00:00',
+        'maxDate'  =>  '2017-01-02 00:00:00',
     ),
-    'product'  =>   array(
-        array( // Semne acceptate / Accepted signs: ee, ne
-            'name'  =>   'laptop', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
-            'sign'  =>  'ee'
-        )
-    ),
+    'customer'  =>  'Client', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
+    'customerSign'  =>  'ee',
+    'product'  =>   'Produs', // daca valoarea elementului este null, 0 sau sirul vid, atunci filtrul nu va fi aplicat / if the element's value is null, 0 or empty string, then it won't be used as a filter
+    'productSign'  =>  'ee',
     'order'  =>   0, // lipsa elementului sau prezenta sa cu valoarile null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
     'invoice'  =>  0, // lipsa elementului sau prezenta sa cu valoarile null, 0 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0 or empty string, then it won't be used as a filter
     'order_status' => 11, // lipsa elementului sau prezenta sa cu valoarile null, 0, 99 sau sirul vid duc la neaplicarea filtrului / if the element is missing or if its value is null, 0, 99 or empty string, then it won't be used as a filter
